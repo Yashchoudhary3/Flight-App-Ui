ae3ca7b9649c128759641e428f7473e36f47eb7e
Yashchoudhary3 <111115497+Yashchoudhary3@users.noreply.github.com>
2025-06-30 14:02:24 +0530

    Update useFlightStatusRealtime.js

--- client/src/hooks/useFlightStatusRealtime.js
+++ client/src/hooks/useFlightStatusRealtime.js
@@ -4,7 +4,7 @@ const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
 
 export function useFlightStatusRealtime(onStatusUpdate) {
   useEffect(() => {
-    const evtSource = new EventSource(`${API_URL}api/flights/stream`);
+    const evtSource = new EventSource(`${API_URL}/api/flights/stream`);
     evtSource.onmessage = (event) => {
       console.log('SSE received:', event.data);
       const updatedFlight = JSON.parse(event.data);
